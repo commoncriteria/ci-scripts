275108d72d746f3aca9e73a074448251864a2519
Kevin G <kjgalli@radium.ncsc.mil>
2020-03-24 12:56:01 +0000

    Fixed typo.

--- create_site.sh
+++ create_site.sh
@@ -141,7 +141,7 @@ function createWebsite {
             <ul class="collapsible" data-collapsible="accordion">
 EOF
         for aa in ${PP_NAMES[@]}; do
-            if [ -d transforms]; then
+            if [ -d transforms ]; then
               pushd transforms > /dev/null
               T_VER="Transforms: $(git branch|cut -c 3-)-$(git rev-parse HEAD|cut -c -8)"
               popd > /dev/null
