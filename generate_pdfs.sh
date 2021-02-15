de31445af059c56a8b1a3f37232b140fe66a7a6b
test <test@example.com>
2021-02-15 16:36:30 -0500

    Hopefully fixed pdf generation...

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -14,31 +14,30 @@ function createPDFs {
     echo $(pwd)/${PP_JOBS_DIR}/${PP_NAME}/${PP_NAME}-release.html
     echo ./${PWD}/${PP_JOBS_DIR}/${PP_NAME}/${PP_NAME}-release.pdf
     
-#    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' /usr/bin/wkhtmltopdf --encoding utf-8 --javascript-delay 15000 http://google.com ./google.pdf
-    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' /usr/bin/wkhtmltopdf \
-            --javascript-delay 15000  \
-            file://$(pwd)/${PP_JOBS_DIR}/${PP_NAME}/${PP_NAME}-release.html?expand=on \
-            ./${PWD}/${PP_JOBS_DIR}/${PP_NAME}/${PP_NAME}-release.pdf;
-#    for aa in $(find ${PP_JOBS_DIR}/${PP_NAME} -mindepth 1 -name '*.html'); do
-#	  echo ${aa}
-#          # Make the PDF
-#    	    xvfb-run --auto-servernum /usr/bin/wkhtmltopdf \
-#		--javascript-delay 15000 \
-#		file://${aa}?expand=on \
-#		./$PP_JOBS_DIR/$PP_NAME/$(basename ${aa%%.html}.pdf);
-#          if [ $? -eq 1 ]; then
-#              exitStatus=1
-#              return $exitStatus
-#          fi
-#    	    xvfb-run --auto-servernum /usr/bin/wkhtmltopdf \
-#		--javascript-delay 15000 --footer-right '[page]' \
-#		file://${aa}?expand=on \
-#		./$PP_JOBS_DIR/$PP_NAME/$(basename ${aa%%.html}-paged.pdf);
-#          if [ $? -eq 1 ]; then
-#              exitStatus=2
-#              return $exitStatus
-#          fi
-#    done
+    #xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' /usr/bin/wkhtmltopdf \
+    #        --javascript-delay 15000  \
+    #        file://$(pwd)/${PP_JOBS_DIR}/${PP_NAME}/${PP_NAME}-release.html?expand=on \
+    #        ./${PWD}/${PP_JOBS_DIR}/${PP_NAME}/${PP_NAME}-release.pdf;
+
+    for aa in $(find ${PP_JOBS_DIR}/${PP_NAME} -mindepth 1 -name '*.html'); do
+          # Make the PDF
+    	    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' /usr/bin/wkhtmltopdf \
+		--javascript-delay 15000 \
+		file://${PWD}/${PP_JOBS_DIR}/${PP_NAME}/${aa}?expand=on \
+		./${PWD}/${PP_JOBS_DIR}/${PP_NAME}/$(basename ${aa%%.html}.pdf);
+          if [ $? -eq 1 ]; then
+              exitStatus=1
+              return $exitStatus
+          fi
+    	    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' /usr/bin/wkhtmltopdf \
+		--javascript-delay 15000 --footer-right '[page]' \
+		file://${PWD}/${PP_JOBS_DIR}/${PP_NAME}/${aa}?expand=on \
+		./${PWD}/${PP_JOBS_DIR}/${PP_NAME}/$(basename ${aa%%.html}-paged.pdf);
+          if [ $? -eq 1 ]; then
+              exitStatus=2
+              return $exitStatus
+          fi
+    done
     return $exitStatus
 }
 
