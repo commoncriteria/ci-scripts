621238fd61d93932c69c659b5978499e8e9a9ae4
kgal <kjgalli@radium.ncsc.mil>
2022-12-06 10:28:47 -0500

    Switched some args around.

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -4,7 +4,7 @@ set -ev
 PP_JOBS_DIR=commoncriteria.github.io/pp
 PP_NAME=$(basename $GITHUB_REPOSITORY)
 
-TOPDF=/usr/bin/wkhtmltopdf --enable-local-file-access --javascript-delay 15000 
+
 # Create PDF files
 function createPDFs {
     exitStatus=0
@@ -12,7 +12,7 @@ function createPDFs {
     for aa in $(find ${PP_JOBS_DIR}/${PP_NAME} -mindepth 1 -name '*.html'); do
           # Make the PDF
     	xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' \
-		 ${TOPDF}\
+		 /usr/bin/wkhtmltopdf --enable-local-file-access --javascript-delay 15000 \
 		file://${PWD}/${aa}?expand=on \
 		${PWD}/${PP_JOBS_DIR}/${PP_NAME}/$(basename ${aa%%.html}.pdf);
           if [ $? -eq 1 ]; then
@@ -20,7 +20,7 @@ function createPDFs {
               return $exitStatus
           fi
     	  xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' \
-		 /usr/bin/wkhtmltopdf --javascript-delay 15000  --enable-local-file-access\		   
+		 /usr/bin/wkhtmltopdf --enable-local-file-access --javascript-delay 15000 \
 		 --footer-right '[page]' \
 		file://${PWD}/${aa}?expand=on \
 		${PWD}/${PP_JOBS_DIR}/${PP_NAME}/$(basename ${aa%%.html}-paged.pdf);
