612bacff80f2abd2847ee4360ae4d63d0d36adb2
kgal <kjgalli@radium.ncsc.mil>
2022-12-06 08:10:10 -0500

    Removed substitution.

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -4,21 +4,23 @@ set -ev
 PP_JOBS_DIR=commoncriteria.github.io/pp
 PP_NAME=$(basename $GITHUB_REPOSITORY)
 
-HTM2PDF=/usr/bin/wkhtmltopdf --javascript-delay 15000 --enable-local-file-access
+
 # Create PDF files
 function createPDFs {
     exitStatus=0
 
     for aa in $(find ${PP_JOBS_DIR}/${PP_NAME} -mindepth 1 -name '*.html'); do
           # Make the PDF
-    	    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' $HTM2PDF\
+    	xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' \
+		 /usr/bin/wkhtmltopdf --javascript-delay 15000 --enable-local-file-access\
 		file://${PWD}/${aa}?expand=on \
 		${PWD}/${PP_JOBS_DIR}/${PP_NAME}/$(basename ${aa%%.html}.pdf);
           if [ $? -eq 1 ]; then
               exitStatus=1
               return $exitStatus
           fi
-    	  xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' $HTM2PDF\
+    	  xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x16' \
+		 /usr/bin/wkhtmltopdf --javascript-delay 15000 --enable-local-file-access\		   
 		 --footer-right '[page]' \
 		file://${PWD}/${aa}?expand=on \
 		${PWD}/${PP_JOBS_DIR}/${PP_NAME}/$(basename ${aa%%.html}-paged.pdf);
